77c7a38de4c5d37184c82e32e39dfe0a5529a794
Yttrium <77174957+Miftau@users.noreply.github.com>
2023-02-23 11:55:20 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+
+"""This file defines the City Model
+It inherits from the BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """The City Model"""
+
+    # Attributes
+    name: str = ""
+    state_id: str = ""
 
